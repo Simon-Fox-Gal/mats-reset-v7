e41222d0113b1f8e93f4a38ba76c2fe67671950d
Simon-Fox-Gal <simonfoxgal@gmail.com>
2025-08-15 14:38:39 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Simple cache-first service worker for v7
+const CACHE = 'mats-reset-v7-cache';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(()=>caches.match('./')))
+  );
+});
